59a5ee319afa0ed31586ed8c06eb524524975a01
nitinp3 <nitinp@iotech.biz>
2021-02-02 16:23:35 +0530

    initial commit api gateway

---
+++ api-gateway-service/src/main/java/com/nnpandey/APIGateWayConfig.java
@@ -0,0 +1,20 @@
+package com.nnpandey;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class APIGateWayConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/**")
+                        .uri("http://localhost:8081/")
+                        .id("microservice_name"))
+                .build();
+    }
+
+}
